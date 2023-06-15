a4bd35bdf8cf899434f647b251f8bfe85ae49d3e
lumi808 <108948664+lumi808@users.noreply.github.com>
2023-06-15 11:03:11 +0600

    updated max tokens number

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -28,9 +28,7 @@ export async function POST(req) {
     model: 'gpt-3.5-turbo',
     messages: messages,
     temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
-    max_tokens: process.env.AI_MAX_TOKENS
-      ? parseInt(process.env.AI_MAX_TOKENS)
-      : 200,
+    max_tokens: 1024,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
